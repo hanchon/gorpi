c3f69dfb0c6655b84a76d2172cbe12cb7beff2a9
hanchon <guillermo.paoletti@gmail.com>
2024-02-13 00:10:30 +0100

    add all the buttons

--- main.go
+++ main.go
@@ -7,32 +7,63 @@ import (
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
+const key1Pin = 21
+const key2Pin = 20
+const key3Pin = 16
+
+const keyUpPin = 6
+const keyDownPin = 19
+const keyLeftPin = 5
+const keyRightPin = 26
+const keyPressPin = 13
+
+func initButton(pinID uint8) *rpio.Pin {
+	pin := rpio.Pin(pinID)
+	pin.Input()
+	pin.PullUp()
+	return &pin
+}
+
 func main() {
 	if err := rpio.Open(); err != nil {
 		panic(err)
 	}
 
-	pin := rpio.Pin(21)
-	pin.Input()
-	pin.PullUp()
+	button1 := initButton(key1Pin)
+	button2 := initButton(key2Pin)
+	button3 := initButton(key3Pin)
+
+	keyUp := initButton(keyUpPin)
+	keyDown := initButton(keyDownPin)
+	keyLeft := initButton(keyLeftPin)
+	keyRight := initButton(keyRightPin)
+	keyPress := initButton(keyPressPin)
 
 	for {
-		if pin.Read() == rpio.Low {
-			fmt.Println("pin 1 low")
+		if button1.Read() == rpio.Low {
+			fmt.Println("button 1 pressed")
 		}
-		time.Sleep(time.Second)
+		if button2.Read() == rpio.Low {
+			fmt.Println("button 2 pressed")
+		}
+		if button3.Read() == rpio.Low {
+			fmt.Println("button 3 pressed")
+		}
+		if keyUp.Read() == rpio.Low {
+			fmt.Println("keyUp pressed")
+		}
+		if keyDown.Read() == rpio.Low {
+			fmt.Println("keyDown pressed")
+		}
+		if keyLeft.Read() == rpio.Low {
+			fmt.Println("keyLeft pressed")
+		}
+		if keyRight.Read() == rpio.Low {
+			fmt.Println("keyRight pressed")
+		}
+		if keyPress.Read() == rpio.Low {
+			fmt.Println("keyPress pressed")
+		}
+		time.Sleep(100 * time.Millisecond)
 	}
-
-	// dev, err := spi.Open(&spi.Devfs{
-	// 	Dev:      "/dev/spidev0.1",
-	// 	Mode:     spi.Mode3,
-	// 	MaxSpeed: 500000,
-	// })
-	// if err != nil {
-	// 	panic(err)
-	// }
-	//     const KEY1_PIN: u8 = 21;
-	// const KEY2_PIN: u8 = 20;
-	// const KEY3_PIN: u8 = 16;
-
 }
